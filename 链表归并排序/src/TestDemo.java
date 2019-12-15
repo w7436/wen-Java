4de27b021016e119caf5a97f23dbda8a2948d7e9
w7436 <3239741254@qq.com>
2019-12-15 15:44:57 +0800

    代码上传

--- 链表归并排序/src/TestDemo.java
+++ 链表归并排序/src/TestDemo.java
@@ -5,7 +5,44 @@
  * @Data 2019/11/30 11:26
  * @Version 1.0
  **/
-public class TestDemo {
-
-
+class ListNode {
+    int  val;
+    ListNode next;
+    public ListNode(int val) {
+        this.val = val;
+    }
 }
+public class TestDemo {
+        public ListNode sortList(ListNode head) {
+            if (head == null || head.next == null)
+                return head;
+            ListNode fast = head.next, slow = head;
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            ListNode tmp = slow.next;
+            slow.next = null;
+            ListNode left = sortList(head);
+            ListNode right = sortList(tmp);
+            ListNode h = new ListNode(0);
+            ListNode res = h;
+            while (left != null && right != null) {
+                if (left.val < right.val) {
+                    h.next = left;
+                    left = left.next;
+                } else {
+                    h.next = right;
+                    right = right.next;
+                }
+                h = h.next;
+            }
+            h.next = left != null ? left : right;
+            return res.next;
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
+
