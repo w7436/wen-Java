e54907d17e51dabe1fb51d957e9aa6afefce102e
w7436 <3239741254@qq.com>
2020-03-10 21:56:04 +0800

    二叉树应用

--- 二叉树/src/TestDemo.java
+++ 二叉树/src/TestDemo.java
@@ -1,5 +1,7 @@
 import sun.reflect.generics.tree.Tree;
 
+import java.util.Stack;
+
 /**
  * @ClassName TestDemo
  * @Description TODO
@@ -16,6 +18,34 @@ public class TestDemo {
         int left_height = maxDepth(root.left);
         int right_height = maxDepth(root.right);
         return java.lang.Math.max(left_height, right_height)+1 ;
+    }
+
+    public static void inOrderNor(TreeNode root){
+        if(null == root){
+            return;
+        }
+        //找root为根的二叉树最左侧的结点，并且保存路径中所有的节点
+        TreeNode cur  = root;
+        Stack<TreeNode> s = new Stack<>();
+
+        while(!s.empty()||cur != null){
+            //当结点不为空时，入栈
+            while (null != cur){
+                s.push(cur);
+                cur = cur.left;
+            }
+            //当结点为空,表示该树已经遍历完成
+            //可以遍历Cur的根节点，也就是栈顶元素
+            cur = s.peek();
+            System.out.println(cur.val);
+            s.pop();
+
+            //以cur为根的二叉树
+            cur = cur.right;
+        }
+
+
+
     }
     public static void main(String[] args) {
         TreeNode root=new TreeNode(1);
