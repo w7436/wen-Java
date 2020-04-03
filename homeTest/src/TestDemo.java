9c0ae9983c98fd78634819ad47e470f3c4d26944
w7436 <3239741254@qq.com>
2020-04-03 22:13:55 +0800

    代码上传

--- homeTest/src/TestDemo.java
+++ homeTest/src/TestDemo.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -180,6 +182,29 @@ public class TestDemo{
 //        }
 //        System.out.println(count);
 //    }
+
+
+    class Solution {
+        private void combSum(int[] nums, int target, int start,
+                             List<Integer> elem, List<List<Integer>> result) {
+            if (target == 0) {
+                result.add(new ArrayList<>(elem));
+                return;
+            }
+            if (target < 0) return;
+            for (int i = start; i < nums.length; ++i) {
+                elem.add(nums[i]);
+                combSum(nums, target-nums[i], i, elem, result);
+                elem.remove(elem.size()-1); // T: O(1)
+            }
+        }
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+            List<List<Integer>> result = new ArrayList<>();
+            List<Integer> elem = new ArrayList<>();
+            combSum(candidates, target, 0, elem, result);
+            return result;
+        }
+    }
 
 //    public static void main(String[] args) {
 
