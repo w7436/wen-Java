6be9ec67d02b8cde7fe178e08b53e950daf8d4e4
w7436 <3239741254@qq.com>
2020-04-06 20:45:56 +0800

    代码上传

---
+++ io_study/src/lesson1/ScannerTest.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+import java.util.Scanner;
+
+/**
+ * @ClassName ScannerTest
+ * @Description TODO
+ * @Author DELL
+ * @Data 2020/4/4 11:09
+ * @Version 1.0
+ **/
+public class ScannerTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
