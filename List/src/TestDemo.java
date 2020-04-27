d3aaadcd5ee35a44e7db6092b22ba25c6dae6f13
w7436 <3239741254@qq.com>
2020-04-27 21:38:07 +0800

    代码

--- List/src/TestDemo.java
+++ List/src/TestDemo.java
@@ -9,44 +9,57 @@ import java.util.List;
  * @Data 2020/1/3 16:01
  * @Version 1.0
  **/
-/*
-* ArrayList检测是否扩容？？怎么扩容？？？？
-* */
+
 public class TestDemo {
     public static void main(String[] args) {
-        List<Integer> L=new LinkedList<>();
-        L.add(1);
-        L.add(2);
-        L.add(3);
-        L.add(4);
-        System.out.println(L);
-        System.out.println(L.size());
-        System.out.println(L.indexOf(4));
+        Integer n = 3;
+        n++;
+        System.out.println(n);
+
+//        int i = 10;
+//// 装箱操作，新建一个 Integer 类型对象，将 i 的值放入对象的某个属性中
+//        Integer ii = Integer.valueOf(i);
+//        Integer ij = new Integer(i);
+//// 拆箱操作，将 Integer 对象中的值取出，放到一个基本数据类型中
+//
+//        int j = ii.intValue();
+//        int i1 = 1;
+//        Integer l1 = i1;
+//
+//        int j = l1;
+//        List<Integer> L=new LinkedList<>();
+//        L.add(1);
+//        L.add(2);
+//        L.add(3);
+//        L.add(4);
+//        System.out.println(L);
+//        System.out.println(L.size());
+//        System.out.println(L.indexOf(4));
 
 
-        //获取任意位置的数据
-        System.out.println(L.get(0));
-    //    System.out.println(L.get(10));
-
-
-        //任意位置的插入
-        L.add(0,0);
-        System.out.println(L);
-
-        L.remove(1);
-
-        if(L.contains(1)){
-            System.out.println("remove:失败");
-
-        }else{
-            System.out.println("remove:成功");
-        }
-        L.add(0);
-        System.out.println(L.indexOf(0));
-        System.out.println(L.lastIndexOf(0));
-
-        //0 2 3 4 0
-        List<Integer> L1=L.subList(1,3);
-        System.out.println(L1);
+//        //获取任意位置的数据
+//        System.out.println(L.get(0));
+//    //    System.out.println(L.get(10));
+//
+//
+//        //任意位置的插入
+//        L.add(0,0);
+//        System.out.println(L);
+//
+//        L.remove(1);
+//
+//        if(L.contains(1)){
+//            System.out.println("remove:失败");
+//
+//        }else{
+//            System.out.println("remove:成功");
+//        }
+//        L.add(0);
+//        System.out.println(L.indexOf(0));
+//        System.out.println(L.lastIndexOf(0));
+//
+//        //0 2 3 4 0
+//        List<Integer> L1=L.subList(1,3);
+//        System.out.println(L1);
     }
 }
