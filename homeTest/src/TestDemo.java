b3a97908942a8cfee2719f4aeb17b6270a67c6c1
w7436 <3239741254@qq.com>
2020-05-02 20:51:09 +0800

    代码

--- homeTest/src/TestDemo.java
+++ homeTest/src/TestDemo.java
@@ -86,15 +86,57 @@ class Student{
     }
 }
 
- class TreeNode {
-     int val;
-     TreeNode left;
-     TreeNode right;
-     TreeNode(int x) {
-         val = x;
-     }
- }
+// class TreeNode {
+//     int val;
+//     TreeNode left;
+//     TreeNode right;
+//     TreeNode(int x) {
+//         val = x;
+//     }
+// }
 public class TestDemo {
+
+
+    /**
+     * 给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。
+     * 一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法
+     * 输入: 12258
+     * 输出: 5
+     * 解释: 12258有5种不同的翻译，分别是"bccfi", "bwfi", "bczi", "mcfi"和"mzi"
+     *
+     *
+     * 利用动态规划
+     * dp[i] = dp[i-1]+dp[i-2]
+     * dp[i] = dp[i-1]
+     */
+    public int translateNum(int num) {
+        String s = String.valueOf(num);
+        int[] dp = new int[s.length()+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2;i <= s.length();i++){
+            int a = Integer.parseInt(s.substring(i-2,i));
+            if(a >= 10 && a <= 25){
+                dp[i] = dp[i-1]+dp[i-2];
+            }else{
+                dp[i] = dp[i-1];
+            }
+        }
+        return dp[s.length()];
+
+
+        // String s = String.valueOf(num);
+        // int a = 1, b = 1;
+        // for(int i = 2; i <= s.length(); i++) {
+        //     String tmp = s.substring(i - 2, i);
+        //     int c = tmp.compareTo("10") >= 0 && tmp.compareTo("25") <= 0 ? a + b : a;
+        //     b = a;
+        //     a = c;
+        // }
+        // return a;
+
+    }
+
     /**
      * 统计一个数字在排序数组中出现的次数。
      * 输入: nums = [5,7,7,8,8,10], target = 8
