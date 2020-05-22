3367b301b1fcfd937fbdd9067cd2a5d6026e2fe3
w7436 <3239741254@qq.com>
2020-05-22 22:03:08 +0800

    代码

--- homeTest/src/Array/Main.java
+++ homeTest/src/Array/Main.java
@@ -2,6 +2,8 @@ package Array;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * @ClassName Main
@@ -11,6 +13,77 @@ import java.util.List;
  * @Version 1.0
  **/
 public class Main {
+    /**
+     * 请设计一个函数，用来判断在一个矩阵中是否存在一条包含某字符串所有字符的路径。路径可以从矩阵中的任意一格开始，
+     * 每一步可以在矩阵中向左、右、上、下移动一格。如果一条路径经过了矩阵的某一格，那么该路径不能再次进入该格子。
+     * 例如，在下面的3×4的矩阵中包含一条字符串“bfce”的路径（路径中的字母用加粗标出）
+     * [["a","b","c","e"],
+     * ["s","f","c","s"],
+     * ["a","d","e","e"]]
+     *
+     * 但矩阵中不包含字符串“abfb”的路径，因为字符串的第一个字符b占据了矩阵中的第一行第二个格子之后，路径不能再次进入这个格子。
+     *
+     * 思想：深度优先算法
+     * @param board
+     * @param word
+     * @return
+     */
+    public boolean exist(char[][] board, String word) {
+        char[] words = word.toCharArray();
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(dfs(board, words, i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+    boolean dfs(char[][] board, char[] word, int i, int j, int k) {
+        if(i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word[k]) return false;
+        if(k == word.length - 1) return true;
+        char tmp = board[i][j];
+        board[i][j] = '/';
+        boolean res = dfs(board, word, i + 1, j, k + 1) || dfs(board, word, i - 1, j, k + 1) ||
+                dfs(board, word, i, j + 1, k + 1) || dfs(board, word, i , j - 1, k + 1);
+        board[i][j] = tmp;
+        return res;
+    }
+
+
+    /**
+     * 输入：nums = [[1,2,3],[4,5,6],[7,8,9]]
+     * 输出：[1,4,2,7,5,3,8,6,9]
+     *
+     * 思想：按照主对角线进行打印
+     * @param nums
+     * @return
+     */
+    public int[] findDiagonalOrder(List<List<Integer>> nums) {
+        int len = 0;
+        Map<Integer,List<Integer>> map = new TreeMap<>();
+        for(int i = 0;i < nums.size();i++) {
+            len += nums.get(i).size(); // 获取最后要返回的数组的长度，即元素个数
+            for(int j = 0;j < nums.get(i).size();j++) {
+                if(map.containsKey(i + j)) {
+                    map.get(i + j).add(nums.get(i).get(j));
+                }
+                else {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums.get(i).get(j));
+                    map.put(i + j, list);
+                }
+            }
+        }
+        int[] ans = new int[len];
+        int index = 0;
+        for(int key : map.keySet()) { // 遍历map
+            List<Integer> list = map.get(key);
+            for(int j = list.size() - 1;j >= 0;j--) { // 根据题目的输出要求确定生成数组中元素的顺序
+                ans[index] = list.get(j);
+                index++;
+            }
+        }
+        return ans;
+    }
     /**
      * [1,2,3,4]
      * [5,6,7,8]
