a145e42a9c14fced96fbc65fd7e246b07cb5acdd
w7436 <3239741254@qq.com>
2020-05-28 22:58:29 +0800

    代码上传

--- homeTest/src/Array/Main.java
+++ homeTest/src/Array/Main.java
@@ -11,6 +11,36 @@ import java.util.*;
  **/
 public class Main {
     /**
+     * 机器人运动范围
+     * 地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。
+     * 一个机器人从坐标 [0, 0] 的格子开始移动，它每次可以向左、右、上、下移动一格（不能移动到方格外），
+     * 也不能进入行坐标和列坐标的数位之和大于k的格子。例如，当k为18时，
+     * 机器人能够进入方格 [35, 37] ，因为3+5+3+7=18。但它不能进入方格 [35, 38]，因为3+5+3+8=19。
+     * 请问该机器人能够到达多少个格子？
+     * 利用深度优先搜索
+     *
+     * 数位和增量
+     * x的数位和为Sx，x+1的数位和为S（x+1）
+     * (x+1)%10 == 0  S(x+1)=Sx - 8 例如19,20的数位和为10,2
+     * (x+1)%10!=0 S(x+1)=Sx+1 ,例如1,2的数位和为1,2
+     *
+     */
+    int m, n, k;
+    boolean[][] visited;
+    public int movingCount(int m, int n, int k) {
+        this.m = m;
+        this.n = n;
+        this.k = k;
+        this.visited = new boolean[m][n];
+        return dfs(0, 0, 0, 0);
+    }
+    public int dfs(int i, int j, int si, int sj) {
+        if(i >= m || j >= n || k < si + sj || visited[i][j])
+            return 0;
+        visited[i][j] = true;//将已经遍历过的置为true
+        return 1 + dfs(i + 1, j, (i + 1) % 10 != 0 ? si + 1 : si - 8, sj) + dfs(i, j + 1, si, (j + 1) % 10 != 0 ? sj + 1 : sj - 8);
+    }
+    /**
      * 是否所有1都至少相隔k个元素
      * 输入：nums = [1,0,0,0,1,0,0,1], k = 2
      * 输出：true
